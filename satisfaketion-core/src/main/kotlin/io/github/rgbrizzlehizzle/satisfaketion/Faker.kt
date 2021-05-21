61e78e60c17bcab720de1f0aaea3b75ab38e0af0
Ryan <ryanbrink@pm.me>
2021-05-21 09:20:16 -0400

    propery tracking

--- satisfaketion-core/src/main/kotlin/io/github/rgbrizzlehizzle/satisfaketion/Faker.kt
+++ satisfaketion-core/src/main/kotlin/io/github/rgbrizzlehizzle/satisfaketion/Faker.kt
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory
 
 class Faker<T> {
 
+  var propertyMap: Map<String, String> = emptyMap()
+
   companion object {
     operator fun <T> invoke(init: Faker<T>.() -> Unit): Faker<T> {
       val builder = Faker<T>()
@@ -16,5 +18,7 @@ class Faker<T> {
 
   operator fun <R> KProperty1<T, R>.invoke(init: Faker<R>.() -> Unit) {
     logger.debug("Look ma I'm in a property 🤠")
+    require(!propertyMap.containsKey(this.name)) { "${this.name} has already been registered" }
+    propertyMap = propertyMap.plus(this.name to "TODO")
   }
 }
--- satisfaketion-core/src/test/kotlin/io/github/rgbrizzlehizzle/satisfaketion/FakerTest.kt
+++ satisfaketion-core/src/test/kotlin/io/github/rgbrizzlehizzle/satisfaketion/FakerTest.kt
@@ -1,15 +1,55 @@
 package io.github.rgbrizzlehizzle.satisfaketion
 
+import io.github.rgbrizzlehizzle.satisfaketion.util.AnotherSimpleClass
 import io.github.rgbrizzlehizzle.satisfaketion.util.SimpleDataClass
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldNotBe
+import io.kotest.matchers.string.shouldStartWith
+import org.junit.jupiter.api.assertThrows
 
 class FakerTest : DescribeSpec({
   describe("Faker Core Functionality") {
     it("Can be instantiated around a data class") {
-      Faker<SimpleDataClass> {
+      // act
+      val fake = Faker<SimpleDataClass> {
         SimpleDataClass::a { }
         SimpleDataClass::b { }
       }
+
+      // assert
+      fake shouldNotBe null
+    }
+    it("Can track invoked properties") {
+      // arrange
+      val fakeA = Faker<SimpleDataClass> {
+        SimpleDataClass::a { }
+        SimpleDataClass::b { }
+      }
+      val fakeB = Faker<AnotherSimpleClass> {
+        AnotherSimpleClass::c { }
+      }
+
+      // act
+      val sizeA = fakeA.propertyMap.size
+      val sizeB = fakeB.propertyMap.size
+
+      // assert
+      assertSoftly {
+        sizeA shouldBeExactly 2
+        sizeB shouldBeExactly 1
+      }
+    }
+    it("Throws an error if a property is invoked multiple times") {
+      // assert
+      val exception = assertThrows<IllegalArgumentException> {
+        Faker<SimpleDataClass> {
+          SimpleDataClass::a { }
+          SimpleDataClass::a { }
+        }
+      }
+      exception.message shouldStartWith "a has already been registered"
     }
   }
 })
