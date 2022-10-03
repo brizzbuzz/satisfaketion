817b7d95036f82bdd446cc2271ef0680942a1fc5
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-10-03 17:54:12 -0500

    fak

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
   kotlin("multiplatform") version "1.7.20" apply false
   id("io.bkbn.sourdough.root") version "0.12.0"
   id("com.github.jakemarsden.git-hooks") version "0.0.2"
-  id("org.jetbrains.kotlinx.kover") version "0.6.0"
+//  id("org.jetbrains.kotlinx.kover") version "0.6.0"
   id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
